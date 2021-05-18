024309b39f39c93b7b296165e7a5ae7e33d3fc11
DevMiguell <buguel2000@gmail.com>
2021-05-18 17:16:38 -0300

    permanecendo subscription e criando o delete

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,20 +1,21 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/client";
-import { fauna } from "../../services/fauna";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/client'
 import { query as q } from 'faunadb'
-import { stripe } from "../../services/stripe";
+
+import { fauna } from '../../services/fauna'
+import { stripe } from '../../services/stripe'
 
 type User = {
   ref: {
     id: string
   }
-  data: { 
-    stripe_custumer_id: string
+  data: {
+    stripe_customer_id: string
   }
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if(req.method === 'POST') {
+  if (req.method === 'POST') {
     const session = await getSession({ req })
 
     const user = await fauna.query<User>(
@@ -26,25 +27,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       )
     )
 
-    let customerId = user.data.stripe_custumer_id
+    let customerId = user.data.stripe_customer_id
 
     if (!customerId) {
-      const stripeCustumer = await stripe.customers.create({
-        email: session.user.email,
-      })  
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email
+        // metadata
+      })
 
       await fauna.query(
         q.Update(
           q.Ref(q.Collection('users'), user.ref.id),
           {
-            data: { 
-              stripe_custumer_id: stripeCustumer.id,
+            data: {
+              stripe_customer_id: stripeCustomer.id
             }
           }
         )
       )
 
-      customerId = stripeCustumer.id
+      customerId = stripeCustomer.id
     }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
@@ -52,7 +54,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
-        { price: 'price_1IsC5mJsf8swKh9ysvNxi36T', quantity: 1}
+        { price: 'price_1IsC5mJsf8swKh9ysvNxi36T', quantity: 1 }
       ],
       mode: 'subscription',
       allow_promotion_codes: true,
